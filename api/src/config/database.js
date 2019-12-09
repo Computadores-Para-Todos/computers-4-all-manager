276ccc760a0b856ea04b8c05119b5e10550df834
lopesboa <lopesboa1@gmail.com>
2019-12-08 21:16:22 -0300

    Setting up sequelize

---
+++ api/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'pc4all',
+  operatorsAliases: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
