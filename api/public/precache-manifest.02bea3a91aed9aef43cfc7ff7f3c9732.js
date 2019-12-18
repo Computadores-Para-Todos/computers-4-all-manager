50fe2ef1ea8f4b2f9a26cd70316b36c016cf8be6
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2019-12-18 07:41:59 -0300

    Teste de deploy Heroku com github actions

---
+++ api/public/precache-manifest.02bea3a91aed9aef43cfc7ff7f3c9732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb2c953b3a04d1a961ffaebe1ac5281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa8540e746e67fd6d37",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e70dbe373d34f4795d31",
+    "url": "/static/js/2.cec56cb1.chunk.js"
+  },
+  {
+    "revision": "0fa8540e746e67fd6d37",
+    "url": "/static/js/main.aac3b81e.chunk.js"
+  },
+  {
+    "revision": "e509363edb0aa96f44a8",
+    "url": "/static/js/runtime-main.cb45f32b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
